83d0eb0fe521764aca81d259a2a026169ef913a7
APermata7 <auliapermatakumala@gmail.com>
2026-06-06 00:43:26 +0700

    refactor(model): rename operator_username column to user

--- app/models/inspection.py
+++ app/models/inspection.py
@@ -13,7 +13,7 @@ class Inspection(Base):
     id = Column(Integer, primary_key=True, index=True)
     inspection_id = Column(String(50), unique=True, index=True, nullable=False)
     part_id = Column(Integer, ForeignKey("part.id"), nullable=False)
-    operator_username = Column(String(50), nullable=False)
+    user = Column(String(50), nullable=False)
     qty_label = Column(Integer, nullable=False)
     n_cv = Column(Integer, nullable=False)
     n_weight = Column(Integer, nullable=False)
